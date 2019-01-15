ad1849ec942ff9efb54f857e8d586fd736c4708e
RoryDong <644828632@qq.com>
2019-01-15 00:40:12 +0000

    Add color options using DOM.

--- allClass.js
+++ allClass.js
@@ -12,10 +12,6 @@ class Parent{
 		this.heightGeneral = heightGeneral || 400;
 		this.t = t || 0 ;
 	};
-
-	createCan(){
-		createCanvas(this.canvWid , this.canvHei);
-	};
 }
 
 /*
@@ -32,7 +28,7 @@ class Cat extends Animal {
 */
 
 class Base extends Parent{
-	constructor(widthGeneral, heightGeneral, t, hue1, br1, hue2, br2, canvWid, canvHei,saturation){
+	constructor(widthGeneral, heightGeneral, t, hue1, br1, hue2, br2, canvWid, canvHei,saturation, rectNum, rectW){
 		super(widthGeneral, heightGeneral, t);
 		this.hue1 = hue1 || 0;
 		this.br1 = br1 || 100;
@@ -41,20 +37,34 @@ class Base extends Parent{
 		this.canvWid = canvWid || 400;
 		this.canvHei = canvHei || 400;
 		this.saturation = saturation || 100;
+		this.rectNum = rectNum || 10;
+		this.rectW = 400 / rectNum || 40;
 	};
+
+	createCan(){
+		createCanvas(this.canvWid , this.canvHei);
+	};	
 
 	setColour(){
 		colorMode(HSB, 360, 100, 100);
 		
-		if (sin(radians(3 * 20 + this.t)) == -1) {
+		if (sin(radians(3 * 20 + this.t)) === -1) {
     		this.hue1 = random(360);
     		this.br1 = random(50, 100);
     	}
 
- 	 	if (sin(radians(3 * 20 + this.t)) == 1) {
+ 	 	if (sin(radians(3 * 20 + this.t)) === 1) {
 	    	this.hue2 = random(360);
     		this.br2 = random(50, 100);
     	}
+	};
+
+	setColour1(){
+		colorMode(HSB, 360, 100, 100);
+		this.hue1 = 0
+		this.br1 = 100
+		this.hue2 = 180
+		this.br2 = 100
 	};
 
 	drawRect(){
@@ -63,11 +73,22 @@ class Base extends Parent{
 		rect(this.widthGeneral / 2, this.heightGeneral / 4, this.widthGeneral, this.heightGeneral / 2);
 		fill(this.hue2, this.saturation, this.br2);
 		rect(this.widthGeneral / 2, this.heightGeneral * 3 / 4, this.widthGeneral, this.heightGeneral / 2);
-
 	};
+
+	draw(){
+		colorMode(RGB, 255, 255, 255);
+  		fill(0, 0, 0);
+		for (var i = 0; i < this.rectNum; i++) {
+    		rect(i * this.rectW + this.rectW / 2, this.heightGeneral / 2 + 100 * sin(radians(i * 20 + this.t)), this.rectW, this.heightGeneral * 0.8);
+  		}
+	};
+
+	incrementT(){
+		this.t++;
+	};	
+
 }
-
-
+/*
 class Move extends Parent{
 	constructor(widthGeneral, heightGeneral, t, rectNum, rectW){
 		super(widthGeneral, heightGeneral, t);
@@ -84,10 +105,10 @@ class Move extends Parent{
 	};
 
 	incrementT(){
-		return this.t++;
+		this.t++;
 	};
 }
-
+*/
 
 class Cover extends Parent{
 	constructor(widthGeneral, heightGeneral, t, circleVertexNum, R, circleColour, circleWid){
@@ -99,25 +120,44 @@ class Cover extends Parent{
 	};
 
 	draw(){
-	fill(255);
-	push();
-  	translate(this.widthGeneral / 2, this.heightGeneral / 2); // 现在corrodinate的（0，0）= （200，200），这一步必须在beginshap前
-  	beginShape();
-  	vertex(-this.widthGeneral / 2, -this.heightGeneral / 2);
-  	vertex(this.widthGeneral / 2, -this.heightGeneral / 2);
-  	vertex(this.widthGeneral / 2, this.heightGeneral / 2);
-  	vertex(-this.widthGeneral / 2, this.heightGeneral / 2);
-  	beginContour();
-  	for (var j = this.circleVertexNum; j > 0; j--) {
-    	vertex(this.R * cos(radians(360 * j / this.circleVertexNum)), this.R * sin(radians(360 * j / this.circleVertexNum)));
-  	}
-  	endContour();
-  	endShape(CLOSE);
-  	pop();
+		fill(255);
+		push();
+  		translate(this.widthGeneral / 2, this.heightGeneral / 2); // 现在corrodinate的（0，0）= （200，200），这一步必须在beginshap前
+  		beginShape();
+  		vertex(-this.widthGeneral / 2, -this.heightGeneral / 2);
+  		vertex(this.widthGeneral / 2, -this.heightGeneral / 2);
+  		vertex(this.widthGeneral / 2, this.heightGeneral / 2);
+  		vertex(-this.widthGeneral / 2, this.heightGeneral / 2);
+  		beginContour();
+  		for (var j = this.circleVertexNum; j > 0; j--) {
+    		vertex(this.R * cos(radians(360 * j / this.circleVertexNum)), this.R * sin(radians(360 * j / this.circleVertexNum)));
+  		}
+  		endContour();
+  		endShape(CLOSE);
+  		pop();
 
-	stroke(this.circleColour);
-  	strokeWeight(this.circleWid);
-  	noFill();
-  	ellipse(this.widthGeneral / 2, this.heightGeneral / 2, this.R * 2, this.R * 2);
+		stroke(this.circleColour);
+  		strokeWeight(this.circleWid);
+  		noFill();
+  		ellipse(this.widthGeneral / 2, this.heightGeneral / 2, this.R * 2, this.R * 2);
 	};
+}
+
+
+
+class controlThePage{
+    constructor(fps, judge1, judge2){
+	this.fps = fps || 60;
+	this.judge1 = judge1 || false;
+	this.judge2 = judge2 || false;
+    };
+
+    setSpeed(fps){
+	this.fps = fps;
+    };
+    
+    changeSpeed(){
+    frameRate(this.fps)
+    };
+
 }
--- myCode.version1.js
+++ myCode.version1.js
@@ -1,54 +1,108 @@
-/* 
-will use class infinied In allClass.js
-
-var c(component);'/ 
-
-(call setup once)
-(calls draw many times())
-*/
-
-let b;
-let m;
-let c;
+let base;
+let cover;
+let settings;
 
 function setup() {
-  b = new Base();
-  b.createCan()
-  m = new Move();
-  c = new Cover();
+  base = new Base();
+  cover = new Cover();
+  settings = new controlThePage();
   rectMode(CENTER);
+  base.createCan();
 }
 
 
 function draw() {
-  b.setColour();
-  b.drawRect();
-  m.draw();
-  m.incrementT();
-  c.draw();
-}
 
-function changeRunTime(){
-  let runningSpeed = document.getElementById("speedRun").value;
-};
+
+  
+  settings.changeSpeed();
+  //frameRate(10);
+  console.log(settings.fps);
+  console.log( frameRate() );
+  
+
+  if( settings.judge1 === false && settings.judge2 === false){
+    base.setColour(); // random 
+  }else if ( settings.judge1 === true) {
+    base.setColour1(); // fixed defult 
+  }else{
+
+  };
+  
+
+  base.drawRect();
+  base.draw();
+  base.incrementT();
+  cover.draw();
+}
 
 
 
 
 
 document.addEventListener("DOMContentLoaded", function(){
-    var cc = document.getElementById("colour");
-    function changeColour(event){
-  let colour = document.getElementById("colour").value;
-  b.setColour(colour);
-    }
-    
-    cc.addEventListener("change", changeColour);
+  // prevent default
+  let y = document.getElementById("theme");
+  y.addEventListener("submit", function (event){ event.preventDefault() });
 
-    var cf = document.getElementById("colour_form");
+  // speed controls
+  function changeRunTime(event){
+    let zz = document.getElementById("inputSpeed").value;
+    settings.setSpeed(zz);
+  };
 
-    cf.addEventListener("submit", function (event){
-  event.preventDefault()});
-});
+  let x = document.getElementById("inputSpeed");
+  x.addEventListener("change", changeRunTime);
+
+  // colour controls
+  // first 2
+  function changeColorFunction(event){
+    let colorOption0 = document.getElementById("color0").checked;
+    if (colorOption0){
+      settings.judge1 = false
+    }else{
+      settings.judge1 = true
+    };
+  };
+
+  let colorElement0 = document.getElementById("color0");
+  colorElement0.addEventListener("change", changeColorFunction);
+  let colorElement1 = document.getElementById("color1");
+  colorElement1.addEventListener("change", changeColorFunction);
+  
+
+  // the range bar
+  let PcolorEle1 = document.getElementById("Pcolor1");
+  PcolorEle1.addEventListener("change", function(){
+    document.getElementById("color2").checked = true;
+    base.hue1 = PcolorEle1.value;
+  });
+
+  let PcolorEle2 = document.getElementById("Pcolor2");
+  PcolorEle2.addEventListener("change", function(){
+    document.getElementById("color2").checked = true;
+    base.br1 = PcolorEle2.value;   
+  });
+
+
+  let PcolorEle3 = document.getElementById("Pcolor3");
+  PcolorEle3.addEventListener("change", function(){
+    document.getElementById("color2").checked = true;
+    base.hue2 = PcolorEle3.value;
+  });
+
+
+  let PcolorEle4 = document.getElementById("Pcolor4");
+  PcolorEle4.addEventListener("change", function(){
+    document.getElementById("color2").checked = true;
+    base.br2 = PcolorEle4.value;
+  });
+
+
+})
+
+
+
+
 
 
