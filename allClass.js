4c4bcbecb5a3d616f27c0c7d0b6af40ee3bf8fde
RoryDong <644828632@qq.com>
2019-01-16 00:40:16 +0000

    add getters and setters, add user interactive rotation function. Debug the color keep changing problem.

--- allClass.js
+++ allClass.js
@@ -2,43 +2,41 @@
 Only defind Class here
 */
 
-//Getters are methods that get and return the internal properties of an object.
 
 
 
 class Parent{
 	constructor(widthGeneral, heightGeneral,t){
-		this.widthGeneral = widthGeneral || 400;
-		this.heightGeneral = heightGeneral || 400;
+		this._widthGeneral = widthGeneral || 400;
+		this._heightGeneral = heightGeneral || 400;
 		this.t = t || 0 ;
 	};
+
+	get widthGeneral(){
+		return this._widthGeneral;
+	};
+
+	get heightGenera(){
+		return this._heightGeneral;
+	}
+
 }
 
-/*
-class Cat extends Animal {
-  constructor(name, usesLitter) {
-    super(name);
-    this._usesLitter = usesLitter;
-  }
-
-  get usesLitter() {
-    return this._usesLitter;
-  }
-}
-*/
 
 class Base extends Parent{
-	constructor(widthGeneral, heightGeneral, t, hue1, br1, hue2, br2, canvWid, canvHei,saturation, rectNum, rectW){
+	constructor(widthGeneral, heightGeneral, t, hue1, br1, hue2, br2, canvWid, canvHei,saturation, rectNum, rectW, angle, angleIndex){
 		super(widthGeneral, heightGeneral, t);
-		this.hue1 = hue1 || 0;
-		this.br1 = br1 || 100;
-		this.hue2 = hue2 || 180;
-		this.br2 = br2 || 100;
-		this.canvWid = canvWid || 400;
-		this.canvHei = canvHei || 400;
+		this._hue1 = hue1 || 0;
+		this._br1 = br1 || 100;
+		this._hue2 = hue2 || 180;
+		this._br2 = br2 || 100;
+		this._canvWid = canvWid || 400;
+		this._canvHei = canvHei || 400;
 		this.saturation = saturation || 100;
 		this.rectNum = rectNum || 10;
 		this.rectW = 400 / rectNum || 40;
+		this._angle = angle || 0;
+		this._angleIndex = angleIndex || 0.01;
 	};
 
 	createCan(){
@@ -49,66 +47,139 @@ class Base extends Parent{
 		colorMode(HSB, 360, 100, 100);
 		
 		if (sin(radians(3 * 20 + this.t)) === -1) {
-    		this.hue1 = random(360);
-    		this.br1 = random(50, 100);
+    		this._hue1 = random(360);
+    		this._br1 = random(50, 100);
     	}
 
  	 	if (sin(radians(3 * 20 + this.t)) === 1) {
-	    	this.hue2 = random(360);
-    		this.br2 = random(50, 100);
+	    	this._hue2 = random(360);
+    		this._br2 = random(50, 100);
     	}
 	};
 
 	setColour1(){
 		colorMode(HSB, 360, 100, 100);
-		this.hue1 = 0
-		this.br1 = 100
-		this.hue2 = 180
-		this.br2 = 100
+		this._hue1 = 0
+		this._br1 = 100
+		this._hue2 = 180
+		this._br2 = 100
+	};
+
+	setColour2(){
+		colorMode(HSB, 360, 100, 100);
+
 	};
 
 	drawRect(){
 		noStroke();
-		fill(this.hue1, this.saturation, this.br1); 
-		rect(this.widthGeneral / 2, this.heightGeneral / 4, this.widthGeneral, this.heightGeneral / 2);
-		fill(this.hue2, this.saturation, this.br2);
-		rect(this.widthGeneral / 2, this.heightGeneral * 3 / 4, this.widthGeneral, this.heightGeneral / 2);
+		fill(this._hue1, this.saturation, this._br1); 
+		rect(this._widthGeneral / 2, this._heightGeneral / 4, this._widthGeneral, this._heightGeneral / 2);
+		fill(this._hue2, this.saturation, this._br2);
+		rect(this._widthGeneral / 2, this._heightGeneral * 3 / 4, this._widthGeneral, this._heightGeneral / 2);
 	};
 
-	draw(){
-		colorMode(RGB, 255, 255, 255);
-  		fill(0, 0, 0);
-		for (var i = 0; i < this.rectNum; i++) {
-    		rect(i * this.rectW + this.rectW / 2, this.heightGeneral / 2 + 100 * sin(radians(i * 20 + this.t)), this.rectW, this.heightGeneral * 0.8);
-  		}
-	};
-
-	incrementT(){
-		this.t++;
+	drawRectRotate(){
+		noStroke();
+		fill(this._hue1, this.saturation, this._br1); 
+		rect(this._widthGeneral / 2 - this.canvWid / 2, this._heightGeneral / 4 - this.canvHei / 2, this._widthGeneral, this._heightGeneral / 2);
+		fill(this._hue2, this.saturation, this._br2);
+		rect(this._widthGeneral / 2 - this.canvWid / 2, this._heightGeneral * 3 / 4 - this.canvWid / 2, this._widthGeneral, this._heightGeneral / 2);
 	};	
 
-}
-/*
-class Move extends Parent{
-	constructor(widthGeneral, heightGeneral, t, rectNum, rectW){
-		super(widthGeneral, heightGeneral, t);
-		this.rectNum = rectNum || 10;
-		this.rectW = 400 / rectNum || 40;
-	};
-
 	draw(){
 		colorMode(RGB, 255, 255, 255);
   		fill(0, 0, 0);
 		for (var i = 0; i < this.rectNum; i++) {
-    		rect(i * this.rectW + this.rectW / 2, this.heightGeneral / 2 + 100 * sin(radians(i * 20 + this.t)), this.rectW, this.heightGeneral * 0.8);
+    		rect(i * this.rectW + this.rectW / 2, this._heightGeneral / 2 + 100 * sin(radians(i * 20 + this.t)), this.rectW, this._heightGeneral * 0.8);
+  		}
+	};
+
+	drawRotate(){
+		colorMode(RGB, 255, 255, 255);
+  		fill(0, 0, 0);
+		for (var i = 0; i < this.rectNum; i++) {
+    		rect(i * this.rectW + this.rectW / 2 - this.canvWid / 2, this._heightGeneral / 2 + 100 * sin(radians(i * 20 + this.t)) - this.canvHei / 2, this.rectW, this._heightGeneral * 0.8);
   		}
 	};
 
 	incrementT(){
 		this.t++;
 	};
+
+	incrementA(){
+		this._angle += this.angleIndex;
+	};
+
+
+	// add the getters and setters for hue1, br1, hue2, br2 and angleIndex.
+	get hue1(){
+		return this._hue1;
+	};
+
+	set hue1(aNumber){
+		if( typeof aNumber === 'number'){
+			this._hue1 = aNumber; 
+		}else{
+			console.log('Invalid input');
+			console.log( typeof aNumber);
+		};
+	}
+
+	get hue2(){
+		return this._hue2;
+	};
+
+	set hue2(aNumber){
+		if( typeof aNumber === 'number'){
+			this._hue2 = aNumber; 
+		}
+	}
+
+	get br1(){
+		return this._br1;
+	};
+
+	set br1(aNumber){
+		if( typeof aNumber === 'number'){
+			this._br1 = aNumber; 
+		}
+	}
+
+	get br2(){
+		return this._br2;
+	};
+
+	set br2(aNumber){
+		if( typeof aNumber === 'number'){
+			this._br2 = aNumber; 
+		}
+	}
+
+	get angleIndex(){
+		return this._angleIndex;
+	};
+
+	set angleIndex(aNumber){
+		if( typeof aNumber === 'number'){
+			this._angleIndex = aNumber; 
+		}
+	}
+
+	// add only getter for canWid, canvHei and angle
+
+	get canvWid(){
+		return this._canvWid;
+	}
+
+	get canvHei(){
+		return this._canvHei;
+	};
+
+	get angle(){
+		return this._angle;
+	}
 }
-*/
+
 
 class Cover extends Parent{
 	constructor(widthGeneral, heightGeneral, t, circleVertexNum, R, circleColour, circleWid){
@@ -120,14 +191,15 @@ class Cover extends Parent{
 	};
 
 	draw(){
-		fill(255);
+		noStroke();
+		fill(255); //white
 		push();
-  		translate(this.widthGeneral / 2, this.heightGeneral / 2); // 现在corrodinate的（0，0）= （200，200），这一步必须在beginshap前
+  		translate(this._widthGeneral / 2, this._heightGeneral / 2); // 现在corrodinate的（0，0）= （200，200），这一步必须在beginshap前
   		beginShape();
-  		vertex(-this.widthGeneral / 2, -this.heightGeneral / 2);
-  		vertex(this.widthGeneral / 2, -this.heightGeneral / 2);
-  		vertex(this.widthGeneral / 2, this.heightGeneral / 2);
-  		vertex(-this.widthGeneral / 2, this.heightGeneral / 2);
+  		vertex(-this._widthGeneral / 2, -this._heightGeneral / 2);
+  		vertex(this._widthGeneral / 2, -this._heightGeneral / 2);
+  		vertex(this._widthGeneral / 2, this._heightGeneral / 2);
+  		vertex(-this._widthGeneral / 2, this._heightGeneral / 2);
   		beginContour();
   		for (var j = this.circleVertexNum; j > 0; j--) {
     		vertex(this.R * cos(radians(360 * j / this.circleVertexNum)), this.R * sin(radians(360 * j / this.circleVertexNum)));
@@ -139,25 +211,50 @@ class Cover extends Parent{
 		stroke(this.circleColour);
   		strokeWeight(this.circleWid);
   		noFill();
-  		ellipse(this.widthGeneral / 2, this.heightGeneral / 2, this.R * 2, this.R * 2);
+  		ellipse(this._widthGeneral / 2, this._heightGeneral / 2, this.R * 2, this.R * 2);
 	};
+
 }
 
 
 
 class controlThePage{
-    constructor(fps, judge1, judge2){
-	this.fps = fps || 60;
-	this.judge1 = judge1 || false;
-	this.judge2 = judge2 || false;
+    constructor(judge1, judge2, rotate){
+	this._judge1 = judge1 || false;
+	this._judge2 = judge2 || false;
+	this._rotate = rotate || false;
+	//console.log(this.fps)
     };
 
-    setSpeed(fps){
-	this.fps = fps;
+    // add the getters and setters for judge1, judge2, rotate.
+    get judge1(){
+    	return this._judge1;
     };
-    
-    changeSpeed(){
-    frameRate(this.fps)
+
+    set judge1(logic){
+    	if( typeof logic === 'boolean'){
+    		this._judge1 = logic;
+    	};
+    };
+
+    get judge2(){
+    	return this._judge2;
+    };
+    set judge2(logic){
+    	if( typeof logic === 'boolean'){
+    		this._judge2 = logic;
+    	};
+    };
+
+    get rotate(){
+    	return this._rotate;
+    };
+    set rotate(logic){
+    	if( typeof logic === 'boolean'){
+    		this._rotate = logic;
+    	};
     };
 
 }
+
+
--- myCode.version1.js
+++ myCode.version1.js
@@ -13,25 +13,41 @@ function setup() {
 
 function draw() {
 
+  
 
   
-  settings.changeSpeed();
-  //frameRate(10);
-  console.log(settings.fps);
-  console.log( frameRate() );
+  //settings.changeSpeed();
+  //frameRate(fr);
+
+  
+  //console.log(fr)
+  //console.log(frameRate())
+  //console.log(settings.fps);
+  //console.log( frameRate() );
   
 
   if( settings.judge1 === false && settings.judge2 === false){
     base.setColour(); // random 
-  }else if ( settings.judge1 === true) {
+  }else if ( settings.judge1 === true ) {
     base.setColour1(); // fixed defult 
   }else{
-
-  };
+    base.setColour2(); // fixed input value
+  };  
   
+  if( settings.rotate === false){
+    base.drawRect();
+    base.draw();
 
-  base.drawRect();
-  base.draw();
+  }else{
+    push();
+    translate( base.canvWid / 2, base.canvHei / 2 );
+    rotate(base.angle);
+    base.drawRectRotate();
+    base.drawRotate();
+    pop();    
+  };
+
+  base.incrementA(); 
   base.incrementT();
   cover.draw();
 }
@@ -47,21 +63,26 @@ document.addEventListener("DOMContentLoaded", function(){
 
   // speed controls
   function changeRunTime(event){
-    let zz = document.getElementById("inputSpeed").value;
-    settings.setSpeed(zz);
+    document.getElementById("Rotation2").checked = true;
+    changeRotationFunction();
+    base.angleIndex = Number(document.getElementById("inputSpeed").value);
   };
 
   let x = document.getElementById("inputSpeed");
   x.addEventListener("change", changeRunTime);
 
+
+
   // colour controls
-  // first 2
+  // first 2 oftions
   function changeColorFunction(event){
     let colorOption0 = document.getElementById("color0").checked;
     if (colorOption0){
-      settings.judge1 = false
+      settings.judge1 = false; 
+      settings.judge2 = false;
     }else{
-      settings.judge1 = true
+      settings.judge1 = true; 
+      settings.judge2 = false;
     };
   };
 
@@ -71,36 +92,62 @@ document.addEventListener("DOMContentLoaded", function(){
   colorElement1.addEventListener("change", changeColorFunction);
   
 
-  // the range bar
+  // the last option->the range bar
   let PcolorEle1 = document.getElementById("Pcolor1");
   PcolorEle1.addEventListener("change", function(){
     document.getElementById("color2").checked = true;
-    base.hue1 = PcolorEle1.value;
+    base.hue1 = Number(PcolorEle1.value);
+    settings.judge1 = false;
+    settings.judge2 = true;
   });
 
   let PcolorEle2 = document.getElementById("Pcolor2");
   PcolorEle2.addEventListener("change", function(){
     document.getElementById("color2").checked = true;
-    base.br1 = PcolorEle2.value;   
+    base.br1 = Number(PcolorEle2.value);   
+    settings.judge1 = false;
+    settings.judge2 = true;
   });
 
 
   let PcolorEle3 = document.getElementById("Pcolor3");
   PcolorEle3.addEventListener("change", function(){
     document.getElementById("color2").checked = true;
-    base.hue2 = PcolorEle3.value;
+    base.hue2 = Number(PcolorEle3.value);
+    settings.judge1 = false;
+    settings.judge2 = true;
   });
 
 
   let PcolorEle4 = document.getElementById("Pcolor4");
   PcolorEle4.addEventListener("change", function(){
     document.getElementById("color2").checked = true;
-    base.br2 = PcolorEle4.value;
+    base.br2 = Number(PcolorEle4.value);
+    settings.judge1 = false;
+    settings.judge2 = true;
   });
 
 
-})
+  // rotation controls
+  function changeRotationFunction(event){
+    let rotationOption1= document.getElementById("Rotation1").checked;
+    if (rotationOption1){
+      settings.rotate = false; 
+    }else{
+      settings.rotate = true; 
+    };
+    console.log(settings.rotate);
+  };
 
+
+  let rotationEle1 = document.getElementById("Rotation1");
+  rotationEle1.addEventListener("change", changeRotationFunction);
+
+  let rotationEle2 = document.getElementById("Rotation2");
+  rotationEle2.addEventListener("change", changeRotationFunction);
+
+
+})
 
 
 
